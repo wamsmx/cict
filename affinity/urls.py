e9f33224a1f4cdd6e4ba7ab5e57d1faa40c86b1a
kyriox <kyriox@gmail.com>
2021-10-08 20:13:00 -0500

    templates update

--- affinity/urls.py
+++ affinity/urls.py
@@ -2,7 +2,20 @@ from django.urls import path, re_path
 
 from . import views
 
+from django.contrib import admin
+from django.urls import path
+from affinity.views import mapmx, frequency,Home, Zonemap, signals, angle
+
 urlpatterns = [
     path('clusters/', views.clusters, name='clusters'),
-    re_path(r'clusters/(?P<d>[\w_]+)/$',views.clusters)
+    re_path(r'clusters/(?P<d>[\w_]+)/$',views.clusters),
+    path('mapmx/', mapmx),
+    path('frequency/', frequency, name='frequency'),
+     path('angle/', angle, name='angle'),
+    path('Home/', Home, name='Home'), 
+    path('Zonemap/', Zonemap, name='Zonemap'),
+    path('signals/', signals),
+
+
 ]
+
