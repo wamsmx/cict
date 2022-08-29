5f5c4a85b6afbf209616091047cf1a5b46c08c2b
kyriox <kyriox@gmail.com>
2022-08-29 15:20:34 -0500

    ringdown

--- roma/views.py
+++ roma/views.py
@@ -17,35 +17,44 @@ def pronyView(request):
     algorithms={0:'Prony', 1:'Matrix Pencil', 3:'ERA'}
     data=pd.DataFrame()
     context={'algorithms':algorithms,
-             'texto':"Hola mundo",
-             'alg':0,}
+             'alg':1,}
+    fn=f"data/Sample_Multi_methods.csv"
+    fn=os.path.join(BASE_DIR,fn)
+    alg=1
     if request.method=='POST':
-        #try:
-            fn=request.FILES['filename']
+            try:
+                fn=request.FILES['filename']
+            except:
+                fn=f"data/Sample_Multi_methods.csv"
+                fn=os.path.join(BASE_DIR,fn)
             alg=int(request.POST.getlist('algorithm')[0])
-            ts=int(request.POST.get('ti',0))
-            tf=int(request.POST.get('tf'))
-            data=pd.read_csv(fn, sep=',')
-            if 'lat' in data.columns and  'lon' in data.columns :
-                data=data.drop(columns=['lat','lon'])
-            if alg==0:
-                hp=int(request.POST.get('order',3))
-                res=Prony(data, ts, tf, hp)
-            else:
-                hp=float(request.POST.get('threshold',0.5))
-                print(ts,tf,hp)
-                if alg==1:
-                    res=MP(data, ts, tf, hp)
-                else:
-                    res=ERA(data, ts, tf, hp)
-            context['alg']=alg
-            df=data.set_index('Node_name')
-            fig = go.Figure()
-            for idx in data.index[1:]:
-                fig.add_trace(go.Scatter(x=data.loc[data.index[0]][ts:tf], y=data.loc[idx][ts:tf],mode='lines',name=idx))
-            context['fig']=fig.to_html()
-            table=pd.DataFrame(res)
-            context['tab']={'table':table.to_html(index=False),'latex':table.to_latex(index=False),'csv':table.to_csv(index=False)}
+    ts=float(request.POST.get('ti',1))
+    tf=float(request.POST.get('tf',5))
+    data=pd.read_csv(fn, sep=',')
+    if 'lat' in data.columns and  'lon' in data.columns :
+        data=data.drop(columns=['lat','lon'])
+    if alg==0:
+        hp=int(request.POST.get('order',10))
+        res,(Ti,Tf)=Prony(data, hp, ts, tf)
+        context['order']=hp
+    else:
+        hp=float(request.POST.get('threshold',0.7))
+        if alg==1:
+            res,(Ti,Tf)=MP(data, hp, ts, tf)
+        else:
+            res,(Ti,Tf)=ERA(data, hp, ts, tf)
+        context['threshold']=hp
+    context['alg']=alg
+    context['ts']=ts
+    context['tf']=tf
+    
+    df=data.set_index('Node_name')
+    fig = go.Figure()
+    for idx in data.index[1:]:
+        fig.add_trace(go.Scatter(x=data.loc[data.index[0]][Ti:Tf], y=data.loc[idx][Ti:Tf],mode='lines',name=idx))
+    context['fig']=fig.to_html()
+    table=pd.DataFrame(res)
+    context['tab']={'table':table.to_html(index=False),'latex':table.to_latex(index=False),'csv':table.to_csv(index=False)}
         #except Exception as e:
         #    print(">>>>>>>>>>>>", e)
             #pass
